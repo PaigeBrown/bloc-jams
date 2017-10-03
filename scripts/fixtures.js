d3c299d73a63d5990073efa51a4d089869d43f66
Paige Brown <brown.emily.paige@gmail.com>
2017-10-03 16:30:35 -0400

    Fixing 18

---
+++ scripts/fixtures.js
@@ -0,0 +1,45 @@
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/img2.jpg',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+ };
+
+ // Another Example Album
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/img3.jpg',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+   };
+
+   var albumDurer = {
+        title: 'Self-Portrait at 28',
+        artist: 'Albrecht Durer',
+        label: 'German Renaissance',
+        year: '1500',
+        albumArtUrl: 'assets/images/img4.jpg',
+        songs: [
+            { title: 'Painting', duration: '4:50' },
+            { title: 'Printmaking', duration: '6:14' },
+            { title: 'Woodcuts', duration: '3:01' },
+            { title: 'Nuremberg', duration: '3:61'},
+            { title: 'Agnes Frey', duration: '2:35'}
+        ]
+    };
